67af95c18ada5e4e6ad3680acc51acbf6097712f
iBug <git@ibugone.com>
2019-10-24 01:30:06 +0800

    almost complete backTCP.h and some .c code

---
+++ btcp.c
@@ -0,0 +1,68 @@
+#include "btcp.h"
+
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int BTSendPacket(BTcpConnection* conn, const void *data, size_t len, uint8_t seq, uint8_t flag) {
+    char buf[1024];
+    int buflen;
+    int socket = conn->socket;
+    struct sockaddr *addr = conn->addr;
+
+    // Tell the remote to close
+    if (len == 0)
+        return sendto(socket, buf, 0, 0, (struct sockaddr*)addr, sizeof(struct sockaddr));
+
+    if (len > 64)
+        len = 64;
+    buflen = sizeof(BTcpHeader) + len;
+
+    BTcpHeader hdr = {
+        .btcp_seq = seq,
+        .data_off = sizeof(BTcpHeader),
+        .flag = flag
+    };
+
+    memcpy(buf + sizeof(BTcpHeader), data, len);
+    return sendto(socket, data, len, 0, (struct sockaddr*)addr, sizeof(struct sockaddr));
+}
+
+int BTAcknowledgePacket(BTcpConnection* conn, uint8_t seq) {
+    int socket = conn->socket;
+    struct sockaddr *addr = conn->addr;
+    BTcpHeader hdr = {
+        .btcp_ack = seq,
+        .data_off = sizeof(BTcpHeader),
+        .flag = F_ACK
+    };
+    return sendto(socket, data, len, 0, (struct sockaddr*)addr, sizeof(struct sockaddr));
+}
+
+int BTSend(BTcpConnection* conn, const void *data, size_t len) {
+}
+
+int BTRecv(BTcpConnection* conn, const void *data, size_t len) {
+    int socket = conn->socket;
+    struct sockaddr *addr = &conn->addr;
+    int addrlen;
+    recvfrom(socket, data, len, addr, &addrlen);
+}
+
+BTcpConnection* BTOpen(unsigned long addr, unsigned short port) {
+    BTcpConnection* conn = malloc(sizeof(BTcpConnection));
+    conn->socket = socket(AF_INET, SOCK_DGRAM, 0);
+    conn->addr.sin_family = AF_INET;
+    conn->addr.sin_addr.s_addr = addr;
+    conn->addr.sin_port = htons(port);
+    return conn;
+}
+
+void BTClose(BTcpConnection* conn) {
+    BTSendPacket(conn, NULL, 0);
+    close(conn->socket);
+    free(conn);
+}
+
+void BTDefaultConfig(BTcpConnection* conn) {
+    conn->config.timeout.tv_usec = 10000;  // default to 10 ms
+}
---
+++ btcp.h
@@ -0,0 +1,63 @@
+#ifndef __BTCP_H
+#define __BTCP_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/time.h>
+
+typedef struct _BTcpConfig {
+    struct timeval timeout;
+} BTcpConfig;
+
+typedef struct _BTcpConnection {
+    int socket;
+    struct sockaddr_in addr;
+    BTcpConfig config;
+} BTcpConnection;
+
+typedef struct _BTcpHeader {
+    uint8_t btcp_sport;  // source port - unused
+    uint8_t btcp_dport;  // destination port - unused
+    uint8_t btcp_seq;    // sequence number
+    uint8_t btcp_ack;    // acknowledgement number
+    uint8_t data_off;    // data offset in bytes
+    uint8_t win_wize;    // window size
+    uint8_t flag;        // flags
+    uint8_t padding1;    // padding byte
+} BTcpHeader;
+
+/********
+* Flags *
+********/
+
+#define F_RETRANSMISSION 0x01
+#define F_EOT            0x02
+#define F_ACK            0x40
+
+/*****************
+* Main Functions *
+*****************/
+
+// Send a stream of data
+int BTSend(BTcpConnection* conn, const void* data, size_t len);
+
+// Receive a stream of data
+int BTRecv(BTcpConnection* conn, const void* data, size_t len);
+
+// Open a backTCP connection
+BTcpConnection* BTOpen(unsigned long addr, unsigned short port);
+
+// Close a backTCP connection
+void BTClose(BTcpConnection* conn);
+
+// Reset a backTCP config to default
+void BTDefaultConfig(BTcpConfig* config);
+
+/*********************
+* Internal Functions *
+*********************/
+
+// Send one single packet
+//int BTSendPacket(BTcpConnection* conn, const void* data, size_t len);
+
+#endif /* __BTCP_H */
